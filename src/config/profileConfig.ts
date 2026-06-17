a20dbf51cd7bab6e9ea34bfa05fedd7b204b1e16
xystart <1850280690@qq.com>
2026-06-17 21:51:41 +0800

    chore(content): sync master@579a199

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,7 +2,7 @@ import type { ProfileConfig } from "../types/config";
 
 // 个人资料配置
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/macos.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "まつざか ゆき",
 	bio: "世界は大きい、君は行かなければならない",
 	typewriter: {
